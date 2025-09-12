d57d97a870c8ed5e3bab102877b617f07fdf58e4
Alberto Casero <kas.appeal@gmail.com>
2025-09-12 14:47:06 +0000

    Refactor App component to improve workspace title management and organize imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+
 import { Box, Container } from '@chakra-ui/react'
-import HomePage from './pages/HomePage'
+import { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/common/Header'
+import { useAuth } from './contexts/AuthContext'
+import HomePage from './pages/HomePage'
+import WorkspacePage from './pages/WorkspacePage'
 
 function App() {
+  const { workspace } = useAuth();
+  useEffect(() => {
+    if (workspace && workspace.name) {
+      document.title = `${workspace.name} - Deita`;
+    } else {
+      document.title = 'Deita - Data exploration for humans';
+    }
+  }, [workspace]);
   return (
     <Box minH="100vh" bg="gray.50">
       <Header />
       <Container maxW="container.xl" py={8}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* Add more routes as needed */}
+          <Route path="/workspaces/:workspaceId" element={<WorkspacePage />} />
         </Routes>
       </Container>
     </Box>
